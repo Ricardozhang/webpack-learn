8e186aac9a6431d8b6e3b5751bf2f045b5871025
zhanghj04 <zhanghj04@fenbi.com>
2020-06-05 11:45:51 +0800

    ADD: 自动生成html文件插件 HtmlWebpackPlugin

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+
+const element = document.createElement('div');
+
+element.innerHTML = 'hello worlk';
+
+document.body.append(element);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -6,5 +7,10 @@ module.exports = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist') // __dirname 被执行js文件的绝对路径
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
 };
