7dc63df21b064889725191d8dc3468073166fd4b
zhanghj04 <zhanghj04@fenbi.com>
2020-06-05 17:57:28 +0800

    ADD: splitChunks

--- webpack.common.js
+++ webpack.common.js
@@ -2,15 +2,17 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: {
         index: './src/index.js',
+        vendor: ['lodash']
     },
-    output: {
-        filename: '[name].bundle.js',
+    output: { // 注： 热替换影响了chunkhash的使用，
+        filename: '[name].[chunkhash].js',
         path: path.resolve(__dirname, 'dist'), // __dirname 被执行js文件的绝对路径
-        chunkFilename: '[name].bundle.js',
+        chunkFilename: '[name].[chunkhash].js',
     },
     plugins: [ // plugins的执行顺序是从上往下
         new CleanWebpackPlugin(),
@@ -19,6 +21,26 @@ module.exports = {
         }),
         new MiniCssExtractPlugin(),
     ],
+    optimization: {
+        splitChunks:{
+            cacheGroups: { // 提取公共部分打包
+                common:{
+                    chunks: 'initial',
+                    name:'common', // 打包后的文件名
+                    minSize: 0, 
+                    minChunks: 2 // 重复2次才能打包到此模块
+                },
+                vendor: {
+                    priority: 1, // 优先级配置，优先匹配优先级更高的规则，不设置的规则优先级默认为0
+                    test: /node_modules/, // 匹配对应文件
+                    chunks: 'initial',
+                    name:'vendor',
+                    minSize: 0,
+                    minChunks: 1
+                }
+            }
+        },
+    },
     module: {
         rules: [{
             test: /\.css$/,
